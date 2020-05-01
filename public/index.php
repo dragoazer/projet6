d173eacda0bc3f2e8026fcca4fa75ce4fa8db6f8
florent <flomat50@gmail.com>
2020-05-01 10:20:36 +0200

    back to work over project 6

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
+	session_start();
 	require "../vendor/autoload.php";
+
+	use projet6\Controller\GeneralController;
+
+	$generalController = new GeneralController();
+
+	if (isset($_GET["action"])) {
+	 	switch ($_GET["action"]) {
+	 		case 'home':
+        		$generalController->displayHome();
+        		break;
+	 	}
+	} else {
+    	$generalController->displayHome();
+    }
